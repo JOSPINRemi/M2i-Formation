5be15e7e5742a35affa5276a24adde00cb9cca71
JOSPINRemi <remi.jospin@gmail.com>
2023-09-19 11:18:11 +0200

    modification TP-Parking

--- Javascript/Tp-Parking/modules/js/Vehicule.js
+++ Javascript/Tp-Parking/modules/js/Vehicule.js
@@ -1,9 +1,12 @@
-class Vehicule {
-  constructor(marque, modele, kilometrage, annee) {
-    this.marque = marque;
-    this.modele = modele;
-    this.kilometrage = kilometrage;
-    this.annee = annee;
+export class Vehicule {
+  constructor(immatriculation, inTime) {
+    this.marque = "marque";
+    this.modele = "modele";
+    this.kilometrage = 0;
+    this.annee = 0;
+    this.immatriculation = immatriculation;
+    this.inTime = inTime;
+    this.outTime = undefined;
   }
 
   display() {
--- Javascript/Tp-Parking/modules/script.js
+++ Javascript/Tp-Parking/modules/script.js
@@ -1,26 +1,58 @@
+import { Vehicule } from "./js/Vehicule.js";
+
 const affichage = document.querySelector(".affichage"),
-immat = document.querySelector("#immat"),
-BtnPayer = document.querySelector("#payer"),
-BtnTicket = document.querySelector("#ticket"),
-valueImmat = immat.value
+  immat = document.querySelector("#immat"),
+  BtnPayer = document.querySelector("#payer"),
+  BtnTicket = document.querySelector("#ticket");
 
-let message,
-parking = []
+let message = "",
+  valueImmat,
+  prix,
+  parking = [];
 
-BtnPayer.addEventListener("click", payer())
-BtnTicket.addEventListener("click", ticket())
+window.onload = setInterval(() => {
+  valueImmat = immat.value;
+}, 1);
 
-function payer() {
-    
+document.addEventListener("click", () => {
+  affichage.innerHTML = message;
+});
+
+BtnPayer.addEventListener("click", () => {
+  for (let i = 0; i < parking.length; i++) {
+    if (parking[i].immatriculation === valueImmat) {
+      parking[i].outTime = new Date();
+      if (timeInParkingMillis(parking[i]) <= 15) {
+        prix = 0.8;
+      } else if (
+        15 < timeInParkingMillis(parking[i]) &&
+        timeInParkingMillis(parking[i]) <= 30
+      ) {
+        prix = 1.3;
+      } else if (
+        30 < timeInParkingMillis(parking[i]) &&
+        timeInParkingMillis(parking[i]) <= 45
+      ) {
+        prix = 1.3;
+      } else if (timeInParkingMillis(parking[i]) > 45) {
+        prix = 6;
+      }
+      message = `Le prix à payer pour le véhicule ${valueImmat} est de ${prix} €`;
+    }
+  }
+  // message = `Le véhicule ${valueImmat} n'existe pas!`;
+  console.log("BtnPayer");
+  console.table(parking);
+});
+
+BtnTicket.addEventListener("click", () => {
+  parking.push(new Vehicule(valueImmat, new Date()));
+  message = `Veuillez prendre votre ticket pour le véhicule ${valueImmat}.`;
+  // message = `Le véhicule ${valueImmat} est déjà dans le parking`;
+  console.log("BtnTicket");
+  console.table(parking);
+});
+
+function timeInParkingMillis(vehicule) {
+  return Math.round((vehicule.outTime - vehicule.inTime) / 1000);
 }
-
-function ticket() {
-    
-}
-
-message = `Le véhicule ${valueImmat} n'existe pas!`;
-message = `Veuillez prendre votre ticket pour le véhicule ${valueImmat}.`;
-message = `Le prix à payer pour le véhicule ${valueImmat} est de {} €`;
-message = `Le véhicule ${valueImmat} est déjà dans le parking`;
-
-affichage.innerHTML = message;
