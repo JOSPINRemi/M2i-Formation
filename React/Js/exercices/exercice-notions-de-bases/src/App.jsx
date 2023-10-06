b67c3194870e9873e8d15826f8aafc6c31f3ed34
JOSPINRemi <remidequarouble@gmail.com>
2023-10-06 19:25:03 +0200

    Ajout des infos à transmettre dans le context

--- React/Js/exercices/exercice-notions-de-bases/src/App.jsx
+++ React/Js/exercices/exercice-notions-de-bases/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { AppContext } from "./contexts/AppContext.js";
+import FormComponent from "./components/FormComponent.jsx";
+import TaskComponent from "./components/TaskComponent.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tasks, setTasks] = useState([]);
+  const STATUS = [
+    {
+      value: "inProgress",
+      label: "En cours",
+    },
+    {
+      value: "over",
+      label: "Finie",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppContext.Provider value={{setTasks, STATUS}}>
+      <FormComponent />
+      {tasks.map((task, id) => (
+        <TaskComponent key={id} />
+        // <button type="button" value={task.status === 'inProgess' ? 'inProgess' : 'over'} onClick={setTasks(prevState => ([...prevState]))}>{task.status === 'inProgess' ? 'En cours' : 'Finie'}</button>
+      ))}
+    </AppContext.Provider>
+  );
 }
 
-export default App
+export default App;
