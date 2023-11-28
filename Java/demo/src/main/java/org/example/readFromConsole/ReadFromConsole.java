b65d43d425a8b5da2861ebd8619be0a8e5f14b3c
JOSPINRemi <remi.jospin@gmail.com>
2023-11-28 11:36:01 +0100

    demo correction erreur Scanner nextInt nextLine

--- Java/demo/src/main/java/org/example/readFromConsole/ReadFromConsole.java
+++ Java/demo/src/main/java/org/example/readFromConsole/ReadFromConsole.java
@@ -3,6 +3,19 @@ package org.example.readFromConsole;
 import java.util.Scanner;
 
 public class ReadFromConsole {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrer un entier :");
+        int num = scanner.nextInt();
+        System.out.println("Nombre saisi : " + num);
+
+        System.out.println("Entrer une chaîne de caractère :");
+        scanner.nextLine();
+        String str = scanner.nextLine();
+        System.out.println("Chaîne de caratère saisie : " + str);
+    }
     public static void getReadWrite() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Donne moi un  : ");
