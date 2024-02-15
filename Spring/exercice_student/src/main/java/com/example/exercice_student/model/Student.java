161b6423b8849052c54e1b7aa19d96c5a7aa7e8e
JOSPINRemi <remi.jospin@gmail.com>
2024-02-15 15:54:34 +0100

    exercice etudiant avec validation sur controller

---
+++ Spring/exercice_student/src/main/java/com/example/exercice_student/model/Student.java
@@ -0,0 +1,37 @@
+package com.example.exercice_student.model;
+
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3)
+    private String lastName;
+
+    @NotNull
+    @Min(10)
+    @Max(90)
+    private Integer age;
+
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    private String email;
+}
