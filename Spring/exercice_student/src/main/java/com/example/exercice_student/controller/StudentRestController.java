c338147473b9f8bf240c29821453a4a67626cb0f
JOSPINRemi <remi.jospin@gmail.com>
2024-02-15 16:34:09 +0100

    exercice etudiant avec validation sur rest controller

--- Spring/exercice_student/src/main/java/com/example/exercice_student/controller/StudentRestController.java
+++ Spring/exercice_student/src/main/java/com/example/exercice_student/controller/StudentRestController.java
@@ -3,7 +3,11 @@ package com.example.exercice_student.controller;
 
 import com.example.exercice_student.model.Student;
 import com.example.exercice_student.service.IStudentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,8 +32,19 @@ public class StudentRestController {
     }
 
     @PostMapping("/add") // http://localhost:8081/api/v1/academy/add
-    public Student createStudent(@RequestBody Student student) {
-        return studentService.createStudent(student);
+    public ResponseEntity<String> createStudent(@Valid @RequestBody Student student, BindingResult result) {
+        if (result.hasErrors()) {
+            StringBuilder errors = new StringBuilder();
+            for (int i = 0; i < result.getAllErrors().size(); i++) {
+                errors.append(result.getAllErrors().get(i).toString());
+                if (i < result.getAllErrors().size() - 1){
+                    errors.append(", ");
+                }
+            }
+            return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+        }
+        studentService.createStudent(student);
+        return new ResponseEntity<>("Etudiant créé avec l'id : " + student.getId() , HttpStatus.CREATED);
     }
 
     @DeleteMapping("/student/{id}") // http://localhost:8081/api/v1/academy/student/x
@@ -38,8 +53,19 @@ public class StudentRestController {
     }
 
     @PutMapping("/student/{id}") // http://localhost:8081/api/v1/academy/student/x
-    public Student updateStudent(@PathVariable Long id, @RequestBody Student updateStudent) {
-        return studentService.updateStudent(id, updateStudent);
+    public ResponseEntity<String> updateStudent(@PathVariable Long id, @Valid @RequestBody Student updateStudent, BindingResult result) {
+        if (result.hasErrors()) {
+            StringBuilder errors = new StringBuilder();
+            for (int i = 0; i < result.getAllErrors().size(); i++) {
+                errors.append(result.getAllErrors().get(i).toString());
+                if (i < result.getAllErrors().size() - 1){
+                    errors.append(", ");
+                }
+            }
+            return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+        }
+        studentService.updateStudent(id, updateStudent);
+        return new ResponseEntity<>("Modification de l'étudiant ok" , HttpStatus.ACCEPTED);
     }
 
 
