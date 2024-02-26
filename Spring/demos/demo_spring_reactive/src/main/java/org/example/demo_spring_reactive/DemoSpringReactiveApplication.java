5d2c11dde62e25e829dcdd4669142a6d03fc4c24
JOSPINRemi <remi.jospin@gmail.com>
2024-02-26 10:10:24 +0100

    mono et flux

---
+++ Spring/demos/demo_spring_reactive/src/main/java/org/example/demo_spring_reactive/DemoSpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_spring_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringReactiveApplication.class, args);
+    }
+
+}
