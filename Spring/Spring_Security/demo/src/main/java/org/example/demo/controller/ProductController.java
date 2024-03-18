ac38ff68d45d7add50291f6ae028ef902cc553d8
JOSPINRemi <remi.jospin@gmail.com>
2024-03-18 17:33:02 +0100

    demo spring security

---
+++ Spring/Spring_Security/demo/src/main/java/org/example/demo/controller/ProductController.java
@@ -0,0 +1,45 @@
+package org.example.demo.controller;
+
+import org.example.demo.dto.ProductDto;
+import org.example.demo.model.Product;
+import org.example.demo.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<Product> save(@RequestBody ProductDto productDto) {
+        return ResponseEntity.ok(productService.save(productDto.getName(), productDto.getPrice()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> get() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> get(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        return ResponseEntity.ok(productService.update(id, productDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ Spring/Spring_Security/demo/src/main/java/org/example/demo/model/Product.java
@@ -0,0 +1,21 @@
+package org.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+}
