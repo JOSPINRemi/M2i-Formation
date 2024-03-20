5e52fa820b891e6c9e6151f4a3632ea093ea4d08
JOSPINRemi <remi.jospin@gmail.com>
2024-03-20 11:53:27 +0100

    demo spring security avec securisation

--- Spring/Spring_Security/demo/src/main/java/org/example/demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.demo.repository;
-
-import org.example.demo.model.Product;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
