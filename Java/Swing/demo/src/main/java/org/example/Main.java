ff560ef6cdeb9f67c77525e02f46a4fe2453cd81
JOSPINRemi <remi.jospin@gmail.com>
2024-04-08 13:53:21 +0200

    demo swing boxlayout

--- Java/Swing/demo/src/main/java/org/example/Main.java
+++ Java/Swing/demo/src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.layout.BorderLayoutDemo;
-import org.example.layout.FlowLayoutDemo;
-import org.example.layout.GridBagLayoutDemo;
-import org.example.layout.GridLayoutDemo;
+import org.example.layout.*;
 
 import javax.swing.*;
 
@@ -12,11 +9,12 @@ public class Main {
         JFrame jFrame = new JFrame("Demo Layout");
         jFrame.setSize(1200, 500);
         jFrame.setLocationRelativeTo(null);
-/*        jFrame.setResizable(false);
+        /*jFrame.setResizable(false);
         jFrame.add(new BorderLayoutDemo());
         jFrame.add(new FlowLayoutDemo().getJPanel());
-        jFrame.add(new GridLayoutDemo().getJPanel());*/
-        jFrame.add(new GridBagLayoutDemo().getJPanel());
+        jFrame.add(new GridLayoutDemo().getJPanel());
+        jFrame.add(new GridBagLayoutDemo().getJPanel());*/
+        jFrame.add(new BoxLayoutDemo().getPanel());
 //        jFrame.pack();
         jFrame.setVisible(true);
     }
